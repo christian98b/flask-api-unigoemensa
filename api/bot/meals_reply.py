09999ed194f13ccbb4781281035653b5478bb980
christian.bruns01 <christian.bruns01@stud.uni-goettingen.de>
2023-06-14 00:40:39 +0200

    text

--- api/bot/meals_reply.py
+++ api/bot/meals_reply.py
@@ -5,8 +5,7 @@ def bot_meals_reply(meals:dict):
             reply += f"_{meal['type']}:_\n *{meal['name']}*\n\n"
         if meal['meal_ingredients'] != '':
             reply += f"Zutaten: {meal['meal_ingredients']}\n"
-        if meal['content'] != 'Mittagsangebot':
+        if meal['content'] != 'Mittagsangebot' or meal['content'] != '':
             reply += f"Angebot: {meal['content']}\n\n"
-        else:
-            reply += "\n"
+        reply += "____________________"
     return reply
