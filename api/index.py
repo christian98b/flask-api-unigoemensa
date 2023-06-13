6415b45a00ba18ccfc71b6b4648a400eeeab88d4
christian.bruns01 <christian.bruns01@stud.uni-goettingen.de>
2023-06-13 17:50:04 +0200

    initial working telegram version

--- api/index.py
+++ api/index.py
@@ -10,11 +10,11 @@ load_dotenv()
 app = flask.Flask(__name__)
 
 bot = telebot.TeleBot(os.environ.get('TELEGRAM_TOKEN'), threaded=False)
-bot.remove_webhook()
-bot.set_webhook(url="https://unigoemensaplan.vercel.app/webhook")
+
 
 @app.route('/')
 def home():
+    print(os.environ.get('TELEGRAM_TOKEN'))
     return 'Make a menu request to /menu?date=YYYY-MM-DD&location=LOCATION'
 
 @app.route("/webhook", methods=["POST"])
@@ -22,7 +22,7 @@ def webhook():
     if flask.request.headers.get("content-type") == "application/json":
         json_string = flask.request.get_data().decode("utf-8")
         update = telebot.types.Update.de_json(json_string)
-        flask.process_new_updates([update])
+        bot.process_new_updates([update])
         return ""
     else:
         app.abort(403)
@@ -35,16 +35,27 @@ def menu():
     return jsonify(mensaMenuAsJson(args.get('date'), args.get('location')))
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=["help", "start"])
+@bot.message_handler(commands=["mensa"])
 def send_welcome(message):
+    menu = mensaMenuAsJson('2023-06-13', 'zentralmensa')
+
+    reply = "Heute in der Zentralmensa gibt es:\n\n"
+    reply += "Ort: " + menu['location'] + "\n"
+    reply += "Datum: " + menu['date'] + "\n\n"
+    for meal in menu['meals']:
+      reply += meal['type'] + ": " + meal['name'] + "\n"
+      reply += "Zutaten: " + meal['meal_ingredients'] + "\n"
+    if meal['content'] != '':
+      reply += "Angebot: " + meal['content'] + "\n"
+      reply += "\n"
+
     bot.reply_to(
-        message,
-        """
-        Welcome to the demo bot. 
-
-        Send a message to get an echo reply
-
-        /gpt <message> - Get a GPT-3 generated reply based on your prompt in gpt.py
-        
-        """,
+        message,reply
     )
+
+
+if __name__ == "__main__":
+    # Remove webhook, it fails sometimes the set if there is a previous webhook
+    bot.remove_webhook()
+    bot.set_webhook(url="https://unigoemensaplan.vercel.app/webhook")
+    #bot.set_webhook(url="https://50b7-2a02-3102-44b5-ffd1-e90d-76f3-a3f9-cc0d.ngrok-free.app/webhook")
