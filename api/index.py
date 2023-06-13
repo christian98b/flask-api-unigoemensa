3cac70a52ad0ee84db50a5178924d47aa289ca33
christian.bruns01 <christian.bruns01@stud.uni-goettingen.de>
2023-06-13 18:53:01 +0200

    webhook edit

--- api/index.py
+++ api/index.py
@@ -18,6 +18,7 @@ def home():
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
+    print("webhook")
     if flask.request.headers.get("content-type") == "application/json":
         json_string = flask.request.get_data().decode("utf-8")
         update = telebot.types.Update.de_json(json_string)
