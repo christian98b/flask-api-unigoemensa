f54b2cd72bb6729b1148039db066750aa7546850
christian.bruns01 <christian.bruns01@stud.uni-goettingen.de>
2023-06-14 00:38:37 +0200

    text formatted

--- api/bot/meals_reply.py
+++ api/bot/meals_reply.py
@@ -1,9 +1,11 @@
 def bot_meals_reply(meals:dict):
     reply = f"Am {meals['date']} gibt es in der {meals['location']}:\n\n"
     for meal in meals['meals']:
-        reply += f"_{meal['type']}:_\n *{meal['name']}*\n"
-        reply += f"Zutaten: {meal['meal_ingredients']}\n"
-        if meal['content'] != '':
+        if meal['name'] != '':
+            reply += f"_{meal['type']}:_\n *{meal['name']}*\n\n"
+        if meal['meal_ingredients'] != '':
+            reply += f"Zutaten: {meal['meal_ingredients']}\n"
+        if meal['content'] != 'Mittagsangebot':
             reply += f"Angebot: {meal['content']}\n\n"
         else:
             reply += "\n"
